8efd6718a739a6fe45913b47fe06a8980c5e749e
vazquezJordy <jordyvazgol@gmail.com>
2021-01-23 17:25:35 -0700

    added a logo compnent and called it in app. also added the image logo and render succefuly, styled was included.

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Logo from "./logo"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <div>
-          Logo
+          <Logo/>
         </div>
       </div>
     );
---
+++ src/components/logo.js
@@ -0,0 +1,13 @@
+import React, {Component } from 'react';
+
+class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img src="/assets/ds_circle_logo.png"/>
+            </div>
+        )
+    }
+    }
+
+    export default Logo;
