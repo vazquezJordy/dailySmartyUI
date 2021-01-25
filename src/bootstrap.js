f9ded7e596e18f38f692260ab10167911040f1cc
vazquezJordy <jordyvazgol@gmail.com>
2021-01-24 22:14:08 -0700

    imported withRouter and passed the reach bar HOC into the search bar to navigate the usur to result. also made the home page to be exact

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -10,14 +10,15 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 import "./style/main.scss";
 
 import Home from "./components/home";
+import Results from "./components/results";
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
        <Switch>
-         <Route path='/' component={Home}/>
-         <Route path='/results' component={Home}/>
+         <Route path='/' exact component={Home}/>
+         <Route path='/results' component={Results}/>
 
        </Switch>
       </BrowserRouter>
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import {Field, reduxForm} from 'redux-form'
 
+import {withRouter} from 'react-router-dom';
+
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
         console.log('tryng to handle submit for query', query)
+
+        this.props.history.push('/results');
     }
 
     renderInput(field) {
@@ -25,5 +29,7 @@ class SearchBar extends Component {
 SearchBar = reduxForm({
     form: 'searchBar'
 })(SearchBar);
+
+SearchBar = withRouter(SearchBar)
 
 export default SearchBar;
