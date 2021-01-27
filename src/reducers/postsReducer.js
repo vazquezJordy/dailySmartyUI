36c51c2de63ffdabd1c61b84189721ee0a3b61f6
vazquezJordy <jordyvazgol@gmail.com>
2021-01-26 21:27:11 -0700

    used axios to fetch post and setup the required reducer. My local host still will not render.

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,19 @@
+import {
+    SET_RECENT_POSTS
+} from '../actions/types'
+
+const INIT_STATE = {
+    posts: [],
+    recentPosts: []
+}
+
+export default function(state = INIT_STAT, action) {
+    switch (action.type) {
+        case SET_RECENT_POSTS:
+            
+            return[...state, recentPosts: action.payload];
+    
+        default:
+            return state;
+    }
+}
