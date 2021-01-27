fb7ca1e50c6d9aed72e2d6ccb0570025c6397130
vazquezJordy <jordyvazgol@gmail.com>
2021-01-26 21:46:52 -0700

    set up redux devtools and fixed reducer: for set recent post and i was also able to fix the bug. with cam fro bootstrap i imported thunk but never used it. :

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -7,11 +7,11 @@ const INIT_STATE = {
     recentPosts: []
 }
 
-export default function(state = INIT_STAT, action) {
+export default function(state = INIT_STATE, action) {
     switch (action.type) {
         case SET_RECENT_POSTS:
-            
-            return[...state, recentPosts: action.payload];
+            const recentPosts = action.payload;
+            return{...state, recentPosts};
     
         default:
             return state;
