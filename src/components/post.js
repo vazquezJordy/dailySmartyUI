40c7967eec362fd5f7e9e7b9547f19ec33233a8e
vazquezJordy <jordyvazgol@gmail.com>
2021-01-27 21:54:02 -0700

    created a post class component and pasted it props for post and it was succefully render and called in recent posts

---
+++ src/components/post.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Post extends Component {
+  render() {
+    return (
+        <li>
+            {this.props.title}
+        </li>
+     )
+  }
+}
+export default Post;
