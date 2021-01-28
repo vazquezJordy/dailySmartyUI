2b4c295e266e03be6b402b5489dd5aba583c07a1
vazquezJordy <jordyvazgol@gmail.com>
2021-01-27 23:28:34 -0700

    made it to where result, home can have a searchbar function via props.

--- src/components/home.js
+++ src/components/home.js
@@ -5,12 +5,18 @@ import SearchBar from './searchBar';
 import ResentPosts from './recentPosts';
 
 export default class Home extends Component {
+
+  handleSearchBarSubmit(query) {
+    console.log('tryng to handle submit for query', query)
+    this.props.history.push('/results');
+}
+
   render() {
     return (
       <div className='app'>
         <div>
           <Logo/>
-          <SearchBar/>
+          <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
           <ResentPosts/>
         </div>
       </div>
