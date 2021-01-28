17335c164e4c87e2f46df9f476957aeca7e8e1df
vazquezJordy <jordyvazgol@gmail.com>
2021-01-27 23:15:30 -0700

    removed h1 tage fro results and added imported the search bar component. added a ternary operation to check if there is asize for the logo if its false it going to default to 105 else it will use the nubmer given to size.

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,9 +2,14 @@ import React, {Component } from 'react';
 
 class Logo extends Component {
     render() {
+        const size = {
+            height:this.props.size ? this.props.size : 105,
+            width:this.props.size ? this.props.size : 105
+            
+        }
         return (
             <div className="logo-main">
-                <img src="/assets/ds_circle_logo.png"/>
+                <img style={size} src="/assets/ds_circle_logo.png"/>
             </div>
         )
     }
