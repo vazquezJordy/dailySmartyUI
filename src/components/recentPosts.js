1374cc5d1f93ec3a40aa7e36ec9ab1b344f25fff
vazquezJordy <jordyvazgol@gmail.com>
2021-01-27 21:40:34 -0700

    added mapstatetoprops and rendered <3 posts on the homepage that belong to recentposts

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,32 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import * as actions from '../actions';
+import * as actions from "../actions";
 
 class RecentPosts extends Component {
-
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchRecentPosts();
   }
+
+  renderPosts = function () {
+    const posts = this.props.recentPosts.map((post, index) => {
+      if (index < 3) {
+        return <li key={index}>{post.title}</li>;
+      }
+    });
+    return posts;
+  };
 
   render() {
     return (
       <div className="recent-posts">
-         <div className="recent-post-wrapper">
-             <div className="recent-posts-heading">
-                 recent posts
-             </div>
-             <ul className="recent-posts-posts">
-                 <li>recnt posts 0 </li>
-                 <li>recnt posts 1 </li>
-                 <li>recnt posts 2 </li>
-                 <li>recnt posts 3 </li>
-             </ul>
-         </div>
+        <div className="recent-post-wrapper">
+          <div className="recent-posts-heading">recent posts</div>
+          <ul className="recent-posts-posts">{this.renderPosts()}</ul>
+        </div>
       </div>
     );
   }
 }
-
-export default connect(null, actions)(RecentPosts);
+function mapStateToProps(state) {
+  return {
+    recentPosts: state.posts.recentPosts,
+  };
+}
+export default connect(mapStateToProps, actions)(RecentPosts);
