4e9e0f3baa79cda05307a50a5003c777b2c6161c
vazquezJordy <jordyvazgol@gmail.com>
2021-01-27 22:44:10 -0700

    added the post topic associated to the post file and it was rendered into the screen

--- src/components/post.js
+++ src/components/post.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 
 class Post extends Component {
+    renderTopics () {
+        let topics = this.props.associated_topics.map((topic, index)=> {
+            return <span className="post-topic" key={index}>{topic}</span>
+        })
+        return topics
+    }
   render() {
     return (
-        <li>
-            {this.props.title}
-        </li>
-     )
+      <li className="recent-post">
+        <div className="recent-post_title">{this.props.title}</div>
+        <div className="recent-post_topics">{this.renderTopics()}</div>
+      </li>
+    );
   }
 }
+
 export default Post;
