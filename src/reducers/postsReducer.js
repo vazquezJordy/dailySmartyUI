5c8a0064c7020506b6ed718baa055efd2602c454
vazquezJordy <jordyvazgol@gmail.com>
2021-01-28 21:59:47 -0700

    added setResultsPost reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,19 +1,22 @@
-import {
-    SET_RECENT_POSTS
-} from '../actions/types'
+import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from "../actions/types";
+
 
 const INIT_STATE = {
-    posts: [],
-    recentPosts: []
-}
+  resultsPosts: [],
+  recentPosts: [],
+};
 
-export default function(state = INIT_STATE, action) {
-    switch (action.type) {
-        case SET_RECENT_POSTS:
-            const recentPosts = action.payload;
-            return{...state, recentPosts};
-    
-        default:
-            return state;
-    }
+export default function (state = INIT_STATE, action) {
+  switch (action.type) {
+    case SET_RECENT_POSTS:
+      const recentPosts = action.payload;
+      return { ...state, recentPosts };
+
+    case SET_RESULTS_POSTS:
+      const resultsPosts = action.payload;
+      return { ...state, resultsPosts };
+
+    default:
+      return state;
+  }
 }
