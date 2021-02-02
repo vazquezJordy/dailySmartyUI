d86c15bd2c570e7d36d45ba11e60c17c30a89fa5
vazquezJordy <jordyvazgol@gmail.com>
2021-02-01 21:38:08 -0700

    imported animations by height and set the state of hight to change on mouseenter/leave

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import AnimateHeight from "react-animate-height";
 
 class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: 0,
+    };
+  }
   renderTopics() {
     let topics = this.props.associated_topics.map((topic, index) => {
       return (
@@ -38,8 +46,16 @@ class Post extends Component {
       return (
         <li className="result-post">
           <div className="result-post__topics">{this.renderTopics()}</div>
-          <div className="result-post__title">{this.props.title}</div>
-          <div className="result-post__links">{this.renderLinks()}</div>
+          <div className="result-post__title">
+            <a href={this.props.url_for_post}
+              onMouseEnter={() => this.setState({height: 70})}
+              onMouseLeave={() => this.setState({height: 0})}
+            >
+              {this.props.title}</a>
+          </div>
+          <AnimateHeight duration={500} height={this.state.height}>
+            <div className="result-post__links">{this.renderLinks()}</div>
+          </AnimateHeight>
         </li>
       );
     }
