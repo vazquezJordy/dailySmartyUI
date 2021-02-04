f9ab38136a1c61cf6c7bdf4fee1e57b42884e9b2
vazquezJordy <jordyvazgol@gmail.com>
2021-02-03 19:26:53 -0700

    styled the search bar component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,33 +1,39 @@
 import React, { Component } from "react";
-import {Field, reduxForm} from 'redux-form'
+import { Field, reduxForm } from "redux-form";
 
-import {withRouter} from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 
 class SearchBar extends Component {
+  handleFormSubmit = function ({ query }) {
+    this.props.onSubmit(query);
+  };
 
-    handleFormSubmit = function({query}) {
-        this.props.onSubmit(query);
-    }
-
-    renderInput(field) {
-        return <input type="text" placeholder="Search DailySmarty" {...field.input}/>
-    }
+  renderInput(field) {
+    return (
+      <input type="text" placeholder="Search DailySmarty" {...field.input} />
+    );
+  }
 
   render() {
-
-    const { handleSubmit} = this.props;
+    const { handleSubmit } = this.props;
     return (
-      <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-          <Field name="query" component={this.renderInput}/>
+      <form
+        className="search-bar"
+        onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}
+      >
+        <div className="search-bar__wrapper">
+          <Field name="query" component={this.renderInput} />
+          <p>Press return to search</p>
+        </div>
       </form>
     );
   }
 }
 
 SearchBar = reduxForm({
-    form: 'searchBar'
+  form: "searchBar",
 })(SearchBar);
 
-SearchBar = withRouter(SearchBar)
+SearchBar = withRouter(SearchBar);
 
 export default SearchBar;
